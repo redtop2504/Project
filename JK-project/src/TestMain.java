9e10b5f3de5530b5a9a9a48f74e5da928c6b7fa3
goott6-2 <goott6-2@goott6-2-PC>
2019-06-13 13:12:24 +0900

    dd

--- JK-project/src/TestMain.java
+++ JK-project/src/TestMain.java
@@ -4,6 +4,9 @@ public class TestMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, GitHub");
+		System.out.println("Hub");
+		System.out.println("itHub");
+		System.out.println("itHdsfsdub");
 	
 	}
 	
